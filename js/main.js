c973248586b32a5182741a1e19ca5b02dbd4aec3
perov_dev <mihailperov90@yandex.ru>
2024-03-27 13:11:37 +0300

    part 3 done

--- js/main.js
+++ js/main.js
@@ -8,15 +8,27 @@ const title = document.querySelector('#title')
 const value = document.querySelector('#value')
 const incomesList = document.querySelector('#incomes-list')
 const expensesList = document.querySelector('#expenses-list')
+const budgetEl = document.querySelector('#budget')
+const totalIncomeEl = document.querySelector('#total-income')
+const totalExpenseEl = document.querySelector('#total-expense')
+const percentsWrapper = document.querySelector('#expense-percents-wrapper')
+const monthEl = document.querySelector('#month')
+const yearEl = document.querySelector('#year')
 
 // FUNCTIONS
+const priceFormatter = new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency: 'RUB',
+    maximumFractionDigits: 0
+})
+
 function insertTestData() {
     const testData = [
         {type: 'inc', title: 'Фриланс', value: 500},
         {type: 'exp', title: 'Бизнес', value: 1500},
         {type: 'exp', title: 'Транспорт', value: 200},
         {type: 'inc', title: 'Рента', value: 3500},
-        {type: 'inc', title: 'Зарплата', value: 100000}
+        {type: 'inc', title: 'Зарплата', value: 10000}
     ]
 
     function getRandomInt(max) {
@@ -35,8 +47,60 @@ function clearForm() {
     form.reset()
 }
 
+function calcBudget() {
+    // Считаем общий доход
+    const totalIncome = budget.reduce(function(total, el) {
+        if (el.type === 'inc') {
+            return total + el.value
+        } else {
+            return total
+        }
+    }, 0)
+
+    // Считаем общий расход
+    const totalExpense = budget.reduce(function(total, el) {
+        if (el.type === 'exp') {
+            return total + el.value
+        } else {
+            return total
+        }    
+    }, 0)
+
+    const totalBudget = totalIncome - totalExpense
+
+    let expensePercents = 0
+    if (totalIncome) {
+        expensePercents = Math.round((totalExpense * 100) / totalIncome)
+    }
+
+    budgetEl.innerHTML = priceFormatter.format(totalBudget)
+    totalIncomeEl.innerHTML = '+ ' + priceFormatter.format(totalIncome)
+    totalExpenseEl.innerHTML = '- ' + priceFormatter.format(totalExpense)
+
+    if (expensePercents) {
+        const html = `<div class="badge">${expensePercents}%</div>`
+        percentsWrapper.innerHTML = html
+    } else {
+        percentsWrapper.innerHTML = ''
+    }
+}
+
+function displayMonth() {
+    const now = new Date()
+    const year = now.getFullYear()
+    
+    const timeFormater = new Intl.DateTimeFormat('ru-RU', { month: 'long' })
+
+    const month = timeFormater.format(now)
+
+    monthEl.innerHTML = month
+    yearEl.innerHTML = year
+}
+
 // ACTIONS
+displayMonth()
 insertTestData()
+calcBudget()
 
 // Добавление новой записи
 form.addEventListener('submit', (e) => {
@@ -72,7 +136,7 @@ form.addEventListener('submit', (e) => {
         id,
         type: type.value,
         title: title.value.trim(),
-        value: value.value
+        value: +value.value
     }
 
     // Добавляем запись в данные
@@ -83,7 +147,7 @@ form.addEventListener('submit', (e) => {
         const html = `<li class="budget-list__item item item--income" data-id="${record.id}">
                         <div class="item__title">${record.title}</div>
                         <div class="item__right">
-                        <div class="item__amount">+ ${record.value}</div>
+                        <div class="item__amount">+ ${priceFormatter.format(record.value)}</div>
                         <button class="item__remove">
                             <img src="./img/circle-green.svg" alt="delete" />
                         </button>
@@ -98,7 +162,7 @@ form.addEventListener('submit', (e) => {
         const html = `<li class="budget-list__item item item--expense" data-id="${record.id}>
                         <div class="item__title">${record.title}</div>
                         <div class="item__right">
-                        <div class="item__amount">- ${record.value}</div>
+                        <div class="item__amount">- ${priceFormatter.format(record.value)}</div>
                         <button class="item__remove">
                             <img src="./img/circle-red.svg" alt="delete" />
                         </button>
@@ -107,6 +171,9 @@ form.addEventListener('submit', (e) => {
 
         expensesList.insertAdjacentHTML('afterbegin', html)
     }
+
+    // Посчитать бюджет
+    calcBudget()
 
     clearForm()
     insertTestData()
@@ -127,5 +194,8 @@ document.body.addEventListener('click', function(e) {
 
         // Remove from page
         recordElement.remove()
+
+        // Посчитать бюджет
+        calcBudget()
     }
 })
